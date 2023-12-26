1f91adcd8b7c219cdbe4c8d83b7478102428a2fe
takaodaze <gin2798@gmail.com>
2023-12-27 05:06:23 +0900

    fix testdata

--- src/test/testData.ts
+++ src/test/testData.ts
@@ -6,7 +6,7 @@ export const expectedData: z.infer<typeof DuelData>[] = [
     duelDate: new Date("2023-12-13 16:01"),
     rating: 1102,
     ratingRange: 79,
-    ratingChangeDirection: "positive",
+    ratingChangeDirection: "+",
     ratingChange: 4.1,
     floor: "F8",
     myselfCharater: "Sol",
